e91382bdb548b1a61896c9dc79d1efdf4d12e473
andreezy777 <andrevasyl@gmail.com>
2020-11-15 22:49:57 +0200

    done

--- bot.py
+++ bot.py
@@ -7,9 +7,11 @@ import telebot
 
 bot = telebot.TeleBot(config.token)
 
+
 @bot.message_handler(content_types=["text"])
-def repeat_all_messages(message): # Название функции не играет никакой роли, в принципе
-    bot.send_message(message.chat.id, message.text)
+def repeat_all_messages(message):  # Название функции не играет никакой роли, в принципе
+    bot.send_message(message.chat.id, "Саша любит Андрея")
+
 
 if __name__ == '__main__':
     bot.polling(none_stop=True)
