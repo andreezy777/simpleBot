82432859c55fc9b1bc8671db9536b8274ffca010
andreezy777 <andrevasyl@gmail.com>
2020-11-16 22:58:13 +0200

    changes to table

--- SQLite3.py
+++ SQLite3.py
@@ -11,21 +11,26 @@ class SQLighter:
         with self.connection:
             return self.cursor.execute('SELECT * FROM Schedule').fetchall()
 
-    def write_to(self, username, name, dayofweek):
+    def write_to(self, chat_id, username, dayofweek):
         sqlite_insert_with_param = """INSERT INTO Schedule
-                                 (User, Name, DayOfWeek) 
+                                 (ChatID, User, DayOfWeek) 
                                  VALUES (?, ?, ?);"""
 
-        data_tuple = (username, name, dayofweek)
+        data_tuple = (chat_id, username, dayofweek)
         self.cursor.execute(sqlite_insert_with_param, data_tuple)
         self.connection.commit()
         print("Python Variables inserted successfully into SqliteDb_developers table")
+
 
     def read_my_data(self, username):
         with self.connection:
             return self.cursor.execute('''SELECT s.DayOfWeek FROM Schedule s
                                         WHERE s.User = ?''', [username]).fetchall()
 
+
+    def getID(self):
+        with self.connection:
+            return self.cursor.execute('''SELECT TOP 1 ID FROM Table ORDER BY ID DESC''')
 
     def close(self):
         """ Закрываем текущее соединение с БД """
--- bot.py
+++ bot.py
@@ -9,14 +9,26 @@ from SQLite3 import SQLighter
 bot = telebot.TeleBot(config.token)
 
 
+@bot.message_handler(commands=['help', 'start'])
+def send_welcome(message):
+    chat_id = message.chat.id
+    msg = bot.reply_to(message, """\
+Я бот-ассистент, напиши день, на который тебе назначить встречу, я его запомню
+""")
+
+
+
+
 @bot.message_handler(content_types=["text"])
 def repeat_all_messages(message):  # Название функции не играет никакой роли, в принципе
     name = message.from_user.first_name
+    chat_id = message.chat.id
     username = message.from_user.username
     bot.send_message(message.chat.id,
                      "{}({}) планирует прогулку в эту/это/этот: {}".format(name, username, message.text))
     db_worker = SQLighter(config.database)
-    write_to_DB = db_worker.write_to(username, name, message.text)
+    #get_ID = db_worker.getID()
+    write_to_DB = db_worker.write_to( chat_id, username, message.text)
     read_from_DB = db_worker.read_my_data(username)
 
     bot.send_message(message.chat.id,
