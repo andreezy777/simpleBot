8f130c5e28ae7222193e6769deb0b03e839e9023
andreezy777 <andrevasyl@gmail.com>
2020-11-17 01:13:15 +0200

    small updates

--- bot.py
+++ bot.py
@@ -1,4 +1,4 @@
-# v0.2
+# v0.4
 #
 #
 
@@ -9,14 +9,13 @@ from SQLite3 import SQLighter
 bot = telebot.TeleBot(config.token)
 day = ''
 
+
 @bot.message_handler(commands=['help', 'start'])
 def send_welcome(message):
     chat_id = message.chat.id
     msg = bot.reply_to(message, """\
 Я бот-ассистент, напиши день, на который тебе назначить встречу, я его запомню
 """)
-
-
 
 
 @bot.message_handler(content_types=["text"])
@@ -33,17 +32,14 @@ def repeat_all_messages(message):  # Название функции не игр
     bot.register_next_step_handler(msg, reply_to_another_user)
     db_worker = SQLighter(config.database)
 
-    write_to_DB = db_worker.write_to( chat_id, user_id , username, message.text)
-    read_from_DB = db_worker.read_my_data(username)
+    write_to_DB = db_worker.write_to(chat_id, user_id, username, message.text)
 
-    bot.send_message(message.chat.id,
-                     "У вас запланированы прогулки на следующие дни: {}".format(''.join(str(x) for x in read_from_DB).
-                                                                                replace('(', '').replace(')', '').
-                                                                                replace('\'', '').replace(',', ', '))[:-2])
     db_worker.close()
+
 
 @bot.message_handler(content_types=['contact'])
 def reply_to_another_user(message):
+    username = message.from_user.username
     db_worker = SQLighter(config.database)
     print(message)
     print(message.contact.user_id)
@@ -54,11 +50,16 @@ def reply_to_another_user(message):
     get_ID = "{}".format(''.join(str(x) for x in get_ID).replace('(', '').replace(')', '').replace('\'', '')[:-1])
     print(get_ID)
     bot.send_message(get_ID, "У вас прогулка с Александрой в {}".format(day))
+    read_from_DB = db_worker.read_my_data(username)
 
+    bot.send_message(message.chat.id,
+                     "У вас запланированы прогулки на следующие дни: {} с {} {}".format(
+                         ''.join(str(x) for x in read_from_DB).
+                         replace('(', '').replace(')', '').
+                         replace('\'', '').replace(',', ', '),
+                         message.contact.first_name,
+                         message.contact.last_name)[:-2])
     db_worker.close()
-
-
-
 
 
 # @bot.message_handler(commands=['getid'])
